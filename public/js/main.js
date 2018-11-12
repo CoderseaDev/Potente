f526a3cd290765366f97ee25533a0f61cb7edd03
nada <nada@codersea.com>
2018-11-12 15:46:00 +0200

    finished crud

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
-const articles=document.getElementById('articles');
-   if(articles) {
-    function deleteuser(e) {
+const users=document.getElementById('users');
+   if(users) {
+    function deleteUser(e) {
         if (confirm('are you sure?')) {
             fetch(`/user/delete/${e.target.getAttribute('data-id')}`, {method: 'DELETE'})
             .then(res => window.location.reload());
